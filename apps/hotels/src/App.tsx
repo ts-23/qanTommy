48eb783b203a4245a9b6735e218bcde360ba874d
Tommy Ling <tommy.ling.business@gmail.com>
2025-01-29 22:33:33 +1100

    inite hotels app

--- apps/hotels/src/App.tsx
+++ apps/hotels/src/App.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import { Card } from "@repo/ui/components";
 import { PrimaryShellLayout } from "@repo/ui/layouts";
 
@@ -6,7 +5,9 @@ export default function Page() {
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
       <PrimaryShellLayout>wegwegweg</PrimaryShellLayout>
-
+      <h1 className="text-3xl font-bold underline bg-green-300">
+        Hello world!
+      </h1>
       <Card title={`title`}>asd </Card>
     </main>
   );
--- apps/hotels/tailwind.config.ts
+++ apps/hotels/tailwind.config.ts
@@ -4,7 +4,7 @@ import type { Config } from "tailwindcss";
 import sharedConfig from "@repo/tailwind-config";
 
 const config: Pick<Config, "content" | "presets"> = {
-  content: ["./app/**/*.tsx"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   presets: [sharedConfig],
 };
 
