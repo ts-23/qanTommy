f007a28bf0c9d4e6500ef0a90ef619a61c1158c4
Tommy Ling <tommy.ling.business@gmail.com>
2025-01-29 23:18:49 +1100

    init card

--- apps/hotels/src/App.tsx
+++ apps/hotels/src/App.tsx
@@ -3,7 +3,15 @@ import { PrimaryShellLayout, ListingLayout } from "@repo/ui/layouts";
 export default function App() {
   return (
     <PrimaryShellLayout logoUrl="/images/qantas-logo.png">
-      <ListingLayout items={[]} />
+      <ListingLayout
+        items={[
+          {
+            title: "asdasd",
+            subtitle: "sdsgd",
+            // imageURL: "https://unsplash.it/145/125/?random",
+          },
+        ]}
+      />
     </PrimaryShellLayout>
   );
 }
--- packages/ui/src/layouts/ListingLayout.tsx
+++ packages/ui/src/layouts/ListingLayout.tsx
@@ -1,9 +1,10 @@
 import type { PropsWithChildren } from "react";
+import { Card } from "../components";
 
 type ListingLayoutItem = {
-  // imageURL?: string;
-  // title?: string;
-  // subtitle?: string;
+  imageURL?: string;
+  title?: string;
+  subtitle?: string;
   // link?: string;
 };
 
@@ -11,9 +12,18 @@ type ListingLayoutProps = { items: ListingLayoutItem[] } & PropsWithChildren;
 
 export function ListingLayout(props: ListingLayoutProps) {
   return (
-    <div className="flex h-32">
-      <div className="bg-red-700 max-w-2xs flex-1">01</div>
-      <div className="bg-green-700 flex-1">2</div>
+    // <div className="flex h-32">
+    //   <div className="bg-red-700 max-w-2xs flex-1">01</div>
+    //   <div className="bg-green-700 flex-1">2</div>
+    // </div>
+    <div>
+      {props.items.map((item) => (
+        <Card
+          title={item.title}
+          subtitle={item.subtitle}
+          imageUrl={item.imageURL}
+        />
+      ))}
     </div>
   );
 }
