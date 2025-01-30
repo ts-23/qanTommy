c75123ed390799572ef8a6d4f81170ad6be5408e
Tommy Ling <tommy.ling.business@gmail.com>
2025-01-31 00:28:44 +1100

    add comments

--- apps/hotels/src/App.tsx
+++ apps/hotels/src/App.tsx
@@ -21,6 +21,8 @@ export default function App() {
   const query = useQuery({
     queryKey: ["news"],
     queryFn: () =>
+      // Ideally we should use a routing library like tanstack router to set the query string like http://localhost:5173?sortBy=foobar
+      // I have done it this way to save time configuring a router library to avoid going beyond the 4-5 hour scope for this assessment.
       getHotels({ sortBy: watch("sortBy") }).then((res) => res.json()), // can use async await, but this is fine because react query handles the error catching
   });
 
@@ -56,6 +58,10 @@ export default function App() {
 
       <ListingLayout
         items={query.data?.results?.map((x: PropertyOffer) => ({
+          // We can use mappers here in a separate package or folder if our project gets larger.
+          // BE-FE types should match and be reused as much as possible.
+          // It is not always the case with companies having FE and BE as separate teams with their own conventions.
+          // Further, design system components and layouts should be reusable without moulding them to fit BE APIs.
           title: x?.property?.title,
           subtitle: x?.property?.address?.join(", "),
           notes: x?.offer?.cancellationOption?.cancellationType,
