4b74f91ad355b7abca8579b3f6dfa4f5abf10354
Tommy Ling <tommy.ling.business@gmail.com>
2025-02-01 16:39:32 +1100

    add comment

--- apps/hotels-api/src/propertyOffer/propertyOffer.controller.spec.ts
+++ apps/hotels-api/src/propertyOffer/propertyOffer.controller.spec.ts
@@ -16,6 +16,8 @@ describe('AppController', () => {
     // we would mock the data if the API is pulling dynamic data source
     it('should return default results', () => {
       const appController = app.get(PropertyOfferService);
+
+      // if the data is dynamic, we could use custom matchers to handle things like changing datetimes and ids that may throw a false positive.
       expect(appController.getPropertyOffers()).toMatchSnapshot();
     });
   });
